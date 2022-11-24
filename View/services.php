9008f1be688de6601d236a9da102221b02937247
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-11-24 09:07:39 +0000

    Update services.php

--- View/services.php
+++ View/services.php
@@ -87,7 +87,7 @@ require("../functions/display_service.php");
                 
                 <!-- <a href="contact.php" class="nav-item nav-link">Contact</a> -->
             </div>
-            <a href="login.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Booking</a>
+            <a href="booking.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Booking</a>
         </div>
     </nav>
     <!-- Navbar End -->
